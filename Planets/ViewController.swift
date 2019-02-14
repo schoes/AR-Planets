3105da5ae6ff9030c36e73c366bc30baa49170cf
Sven Schoeni <sven.schoeni@gmail.com>
2019-02-14 09:22:08 +0100

    Initial Commit

---
+++ Planets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by Sven Schöni on 14.02.19.
+//  Copyright © 2019 Sven Schoeni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
