94f158a01b7c5902a75f2a7f0ffed660874a732d
Sven Schoeni <sven.schoeni@gmail.com>
2019-02-15 11:21:48 +0100

    add moon to earth center point

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         let earthCenterPoint = SCNNode();
         earthCenterPoint.position = sun.position
         let moonCenterPoint = SCNNode();
-        moonCenterPoint.position = sun.position
+
         let venusCenterPoint = SCNNode();
         venusCenterPoint.position = sun.position
         let mercuryCenterPoint = SCNNode();
@@ -41,7 +41,8 @@ class ViewController: UIViewController {
         let venus = planet(geometry: SCNSphere(radius: 0.1), diffuse: UIImage(named: "venus")! ,specular: nil,emission:UIImage(named: "venus atmos"), normal: nil, position:  SCNVector3(0.7,0,0))
         let mercury = planet(geometry: SCNSphere(radius: 0.05), diffuse: UIImage(named: "mercury")! ,specular: nil,emission:nil, normal: nil, position:  SCNVector3(0.5,0,0))
         let earth = planet(geometry: SCNSphere(radius: 0.2), diffuse: UIImage(named: "earth day")!, specular: UIImage(named: "earth specular"), emission: UIImage(named: "earth clouds"), normal: UIImage(named: "earth textures"), position: SCNVector3(1.1,0,0))
-        let moon = planet(geometry: SCNSphere(radius: 0.02), diffuse: UIImage(named: "moon")!, specular: nil, emission: nil, normal: nil, position: SCNVector3(1.35,0,0))
+        moonCenterPoint.position = earth.position
+        let moon = planet(geometry: SCNSphere(radius: 0.02), diffuse: UIImage(named: "moon")!, specular: nil, emission: nil, normal: nil, position: SCNVector3(0.35,0,0))
         let mars = planet(geometry: SCNSphere(radius: 0.05), diffuse: UIImage(named: "mars")! ,specular:
             nil,emission:nil, normal: nil, position:  SCNVector3(1.5,0,0))
         let jupiter = planet(geometry: SCNSphere(radius: 0.3), diffuse: UIImage(named: "jupiter")! ,specular: nil,emission:nil, normal: nil, position:  SCNVector3(2,0,0))
@@ -49,16 +50,16 @@ class ViewController: UIViewController {
         let uranus = planet(geometry: SCNSphere(radius: 0.22), diffuse: UIImage(named: "uranus")! ,specular: nil,emission:nil, normal: nil, position:  SCNVector3(3.5,0,0))
         let neptune = planet(geometry: SCNSphere(radius: 0.22), diffuse: UIImage(named: "neptune")! ,specular: nil,emission:nil, normal: nil, position:  SCNVector3(4.5,0,0))
     
-        let earthRotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 9))
-        let moonRotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8.5))
-        let venusRotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration:10))
-        let mercuryRotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 11))
-        let marsRotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8))
-        let jupiterRotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 7))
-        let saturnRotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 6))
-        let uranusRotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 5))
-        let neptuneRotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 4))
-        let sunRotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 15))
+        let earthRotation = planetRotation(duration: 9)
+        let moonRotation = planetRotation(duration: 8.5)
+        let venusRotation = planetRotation(duration: 10)
+        let mercuryRotation = planetRotation(duration: 11)
+        let marsRotation = planetRotation(duration: 8)
+        let jupiterRotation = planetRotation(duration: 7)
+        let saturnRotation = planetRotation(duration: 6)
+        let uranusRotation = planetRotation(duration: 5)
+        let neptuneRotation = planetRotation(duration: 4)
+        let sunRotation = planetRotation(duration: 15)
         
         earthCenterPoint.addChildNode(earth)
         earthCenterPoint.runAction(earthRotation)
@@ -93,7 +94,6 @@ class ViewController: UIViewController {
         
         self.sceneView.scene.rootNode.addChildNode(sun);
         self.sceneView.scene.rootNode.addChildNode(earthCenterPoint);
-        self.sceneView.scene.rootNode.addChildNode(moonCenterPoint);
         self.sceneView.scene.rootNode.addChildNode(venusCenterPoint);
         self.sceneView.scene.rootNode.addChildNode(mercuryCenterPoint);
         self.sceneView.scene.rootNode.addChildNode(marsCenterPoint)
@@ -111,6 +111,10 @@ class ViewController: UIViewController {
         planet.position = position
         return planet
         
+    }
+    
+    func planetRotation(duration:TimeInterval)-> SCNAction{
+        return SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: duration))
     }
 
 
